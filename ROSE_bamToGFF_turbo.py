24c76039bcc2d3bf479bec09b277c170ff999c71
Charles Lin <charles.y.lin@gmail.com>
2013-09-20 16:11:20 -0400

    updated turbo versions of rose

--- ROSE_bamToGFF_turbo.py
+++ ROSE_bamToGFF_turbo.py
@@ -91,7 +91,8 @@ def mapBamToGFF(bamFile,gff,sense = '.',extension = 200,rpm = False,clusterGram 
             bamSense = '.'
         #using the bamLiquidator to get the readstring            
         #print('using nBin of %s' % nBin)
-        bamCommand = "bamliquidator %s %s %s %s %s %s %s" % (bamFile,line[0],gffLocus.start(),gffLocus.end(),bamSense,nBin,extension)
+        bamliquidatorString = '/usr/bin/bamliquidator'
+        bamCommand = "%s %s %s %s %s %s %s %s" % (bamliquidatorString,bamFile,line[0],gffLocus.start(),gffLocus.end(),bamSense,nBin,extension)
         #print(bamCommand)
         getReads = subprocess.Popen(bamCommand,stdin = subprocess.PIPE,stderr = subprocess.PIPE,stdout = subprocess.PIPE,shell = True)
         readString = getReads.communicate()
