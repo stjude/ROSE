0c44a42c8f50c905c72d7ecac51140bb5c7c98b1
Adetunji <madetunj@splprhpc06.cm.cluster>
2019-12-18 10:52:59 -0600

    modified for St. Jude, Abraham's lab use

---
+++ src/ROSE_callSuper.R
@@ -0,0 +1,175 @@
+#!/usr/bin/env Rscript
+#============================================================================
+#==============SUPER-ENHANCER CALLING AND PLOTTING FUNCTIONS=================
+#============================================================================
+
+#This function calculates the cutoff by sliding a diagonal line and finding where it is tangential (or as close as possible)
+calculate_cutoff <- function(inputVector, drawPlot=TRUE,...){
+ 	inputVector <- sort(inputVector)
+	inputVector[inputVector<0]<-0 #set those regions with more control than ranking equal to zero
+	slope <- (max(inputVector)-min(inputVector))/length(inputVector) #This is the slope of the line we want to slide. This is the diagonal.
+	xPt <- floor(optimize(numPts_below_line,lower=1,upper=length(inputVector),myVector= inputVector,slope=slope)$minimum) #Find the x-axis point where a line passing through that point has the minimum number of points below it. (ie. tangent)
+	y_cutoff <- inputVector[xPt] #The y-value at this x point. This is our cutoff.
+	
+	if(drawPlot){  #if TRUE, draw the plot
+		plot(1:length(inputVector), inputVector,type="l",...)
+		b <- y_cutoff-(slope* xPt)
+		abline(v= xPt,h= y_cutoff,lty=2,col=8)
+		points(xPt,y_cutoff,pch=16,cex=0.9,col=2)
+		abline(coef=c(b,slope),col=2)
+		title(paste("x=",xPt,"\ny=",signif(y_cutoff,3),"\nFold over Median=",signif(y_cutoff/median(inputVector),3),"x\nFold over Mean=",signif(y_cutoff/mean(inputVector),3),"x",sep=""))
+		axis(1,sum(inputVector==0),sum(inputVector==0),col.axis="pink",col="pink") #Number of regions with zero signal
+	}
+	return(list(absolute=y_cutoff,overMedian=y_cutoff/median(inputVector),overMean=y_cutoff/mean(inputVector)))
+}
+
+#this is an accessory function, that determines the number of points below a diagnoal passing through [x,yPt]
+numPts_below_line <- function(myVector,slope,x){
+	yPt <- myVector[x]
+	b <- yPt-(slope*x)
+	xPts <- 1:length(myVector)
+	return(sum(myVector<=(xPts*slope+b)))
+}
+
+
+convert_stitched_to_bed <- function(inputStitched,trackName,trackDescription,outputFile,splitSuper=TRUE,score=c(),superRows=c(),baseColor="0,0,0",superColor="255,0,0"){
+	outMatrix <- matrix(data="",ncol=4+ifelse(length(score)==nrow(inputStitched),1,0),nrow=nrow(inputStitched))
+	
+	outMatrix[,1] <- as.character(inputStitched$CHROM)
+	outMatrix[,2] <- as.character(inputStitched$START)
+	outMatrix[,3] <- as.character(inputStitched$STOP)
+	outMatrix[,4] <- as.character(inputStitched$REGION_ID)
+	if(length(score)==nrow(inputStitched)){
+		score <- rank(score,ties.method="first")
+		score <- length(score)-score+1  #Stupid rank only does smallest to largest. 
+		outMatrix[,5] <- as.character(score)
+	}
+	trackDescription <- paste(trackDescription,"\nCreated on ",format(Sys.time(), "%b %d %Y"),collapse="",sep="")
+	trackDescription <- gsub("\n","\t", trackDescription)
+	tName <- gsub(" ","_",trackName)
+	cat('track name="', tName,'" description="', trackDescription,'" itemRGB=On color=',baseColor,"\n",sep="",file=outputFile)
+	write.table(file= outputFile,outMatrix,sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE,append=TRUE)
+	if(splitSuper==TRUE){
+		cat("\ntrack name=\"Super_", tName,'" description="Super ', trackDescription,'" itemRGB=On color=', superColor,"\n",sep="",file=outputFile,append=TRUE)
+		write.table(file= outputFile,outMatrix[superRows,],sep="\t",quote=FALSE,row.names=FALSE,col.names=FALSE,append=TRUE)
+	}
+}
+
+writeSuperEnhancer_table <- function(superEnhancer,description,outputFile,additionalData=NA){
+	description <- paste("#",description,"\nCreated on ",format(Sys.time(), "%b %d %Y"),collapse="",sep="")
+	description <- gsub("\n","\n#",description)
+	cat(description,"\n",file=outputFile)
+	if(is.matrix(additionalData)){
+		if(nrow(additionalData)!=nrow(superEnhancer)){
+			warning("Additional data does not have the same number of rows as the number of super enhancers.\n--->>> ADDITIONAL DATA NOT INCLUDED <<<---\n")
+		}else{
+			superEnhancer <- cbind(superEnhancer,additionalData)
+			superEnhancer = superEnhancer[order(superEnhancer$enhancerRank),]
+			
+		}
+	}
+	write.table(file=outputFile,superEnhancer,sep="\t",quote=FALSE,row.names=FALSE,append=TRUE)
+}
+
+
+
+#============================================================================
+#===================SUPER-ENHANCER CALLING AND PLOTTING======================
+#============================================================================
+
+
+
+args <- commandArgs()
+
+#print('THESE ARE THE ARGUMENTS')
+#print(args)
+
+#ARGS
+outFolder = args[6] #3
+enhancerFile = args[7] #4
+enhancerName = args[8] #5
+wceName = args[9] #6
+
+
+
+#Read enhancer regions with closestGene columns
+stitched_regions <- read.delim(file= enhancerFile,sep="\t")
+
+
+#perform WCE subtraction. Using pipeline table to match samples to proper background. 
+rankBy_factor = colnames(stitched_regions)[7]
+
+prefix = unlist(strsplit(rankBy_factor,'_'))[1]
+
+if(wceName == 'NONE'){
+	
+	
+	rankBy_vector = as.numeric(stitched_regions[,7])
+	
+}else{
+	wceName = colnames(stitched_regions)[8]
+	print('HERE IS THE WCE NAME')
+	print(wceName)
+	
+	rankBy_vector = as.numeric(stitched_regions[,7])-as.numeric(stitched_regions[,8])
+}	
+	
+
+#SETTING NEGATIVE VALUES IN THE rankBy_vector to 0
+
+rankBy_vector[rankBy_vector < 0] <- 0
+
+
+#FIGURING OUT THE CUTOFF
+
+cutoff_options <- calculate_cutoff(rankBy_vector, drawPlot=FALSE,xlab=paste(rankBy_factor,'_enhancers'),ylab=paste(rankByFactor,' Signal','- ',wceName),lwd=2,col=4)
+
+
+#These are the super-enhancers
+superEnhancerRows <- which(rankBy_vector> cutoff_options$absolute)
+typicalEnhancers = setdiff(1:nrow(stitched_regions),superEnhancerRows)
+enhancerDescription <- paste(enhancerName," Enhancers\nCreated from ", enhancerFile,"\nRanked by ",rankBy_factor,"\nUsing cutoff of ",cutoff_options$absolute," for Super-Enhancers",sep="",collapse="")
+
+
+#MAKING HOCKEY STICK PLOT
+plotFileName = paste(outFolder,enhancerName,'_Plot_points.png',sep='')
+png(filename=plotFileName,height=600,width=600)
+signalOrder = order(rankBy_vector,decreasing=TRUE)
+if(wceName == 'NONE'){
+	plot(length(rankBy_vector):1,rankBy_vector[signalOrder], col='red',xlab=paste(rankBy_factor,'_enhancers'),ylab=paste(rankBy_factor,' Signal'),pch=19,cex=2)	
+	
+}else{
+	plot(length(rankBy_vector):1,rankBy_vector[signalOrder], col='red',xlab=paste(rankBy_factor,'_enhancers'),ylab=paste(rankBy_factor,' Signal','- ',wceName),pch=19,cex=2)
+}
+abline(h=cutoff_options$absolute,color='grey',lty=2)
+abline(v=length(rankBy_vector)-length(superEnhancerRows),color='grey',lty=2)
+lines(length(rankBy_vector):1,rankBy_vector[signalOrder],lwd=4, col='red')
+text(0,0.8*max(rankBy_vector),paste(' Cutoff used: ',cutoff_options$absolute,'\n','Super-Enhancers identified: ',length(superEnhancerRows)),pos=4)
+
+dev.off()
+
+
+
+
+#Writing a bed file
+bedFileName = paste(outFolder,enhancerName,'_Enhancers_withSuper.bed',sep='')
+convert_stitched_to_bed(stitched_regions,paste(rankBy_factor,"Enhancers"), enhancerDescription,bedFileName,score=rankBy_vector,splitSuper=TRUE,superRows= superEnhancerRows,baseColor="0,0,0",superColor="255,0,0")
+
+
+
+#This matrix is just the super_enhancers
+true_super_enhancers <- stitched_regions[superEnhancerRows,]
+
+additionalTableData <- matrix(data=NA,ncol=2,nrow=nrow(stitched_regions))
+colnames(additionalTableData) <- c("enhancerRank","isSuper")
+additionalTableData[,1] <- nrow(stitched_regions)-rank(rankBy_vector,ties.method="first")+1
+additionalTableData[,2] <- 0
+additionalTableData[superEnhancerRows,2] <- 1
+
+
+#Writing enhancer and super-enhancer tables with enhancers ranked and super status annotated
+enhancerTableFile = paste(outFolder,enhancerName,'_AllEnhancers.table.txt',sep='')
+writeSuperEnhancer_table(stitched_regions, enhancerDescription,enhancerTableFile, additionalData= additionalTableData)
+
+superTableFile = paste(outFolder,enhancerName,'_SuperEnhancers.table.txt',sep='')
+writeSuperEnhancer_table(true_super_enhancers, enhancerDescription,superTableFile, additionalData= additionalTableData[superEnhancerRows,])
---
+++ src/ROSE_geneMapper.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python2
+#130428
+
+#ROSE_geneMapper.py
+
+#main method wrapped script to take the enhancer region table output of ROSE_Main and map genes to it
+#will create two outputs a gene mapped region table where each row is an enhancer
+#and a gene table where each row is a gene
+#does this by default for super-enhancers only
+
+import sys
+
+
+
+import ROSE_utils
+
+
+import os
+
+from string import upper,join
+
+from collections import defaultdict
+
+
+#==================================================================
+#====================MAPPING GENES TO ENHANCERS====================
+#==================================================================
+
+
+
+def mapEnhancerToGene(annotFile,enhancerFile,transcribedFile='',uniqueGenes=True,byRefseq=False):
+    
+    '''
+    maps genes to enhancers. if uniqueGenes, reduces to gene name only. Otherwise, gives for each refseq
+    '''
+    print "Herp"
+    startDict = ROSE_utils.makeStartDict(annotFile)
+    print "Derp"
+    enhancerTable = ROSE_utils.parseTable(enhancerFile,'\t')
+
+
+
+
+
+    if len(transcribedFile) > 0:
+        transcribedTable = ROSE_utils.parseTable(transcribedFile,'\t')
+        transcribedGenes = [line[1] for line in transcribedTable]
+    else:
+        transcribedGenes = startDict.keys()
+
+    print('MAKING TRANSCRIPT COLLECTION')
+    transcribedCollection = ROSE_utils.makeTranscriptCollection(annotFile,0,0,500,transcribedGenes)
+
+
+    print('MAKING TSS COLLECTION')
+    tssLoci = []
+    for geneID in transcribedGenes:
+        tssLoci.append(ROSE_utils.makeTSSLocus(geneID,startDict,0,0))
+
+
+    #this turns the tssLoci list into a LocusCollection
+    #50 is the internal parameter for LocusCollection and doesn't really matter
+    tssCollection = ROSE_utils.LocusCollection(tssLoci,50)
+
+
+
+    geneDict = {'overlapping':defaultdict(list),'proximal':defaultdict(list)}
+    #list of all genes that appear in this analysis
+    overallGeneList = []
+
+    #set up the output tables
+    #first by enhancer
+    enhancerToGeneTable = [enhancerTable[5][0:6]+['OVERLAP_GENES','PROXIMAL_GENES','CLOSEST_GENE'] + enhancerTable[5][-2:]]
+    
+    #next by gene
+    geneToEnhancerTable = [['GENE_NAME','REFSEQ_ID','PROXIMAL_ENHANCERS']]
+
+
+    for line in enhancerTable[6:]:
+
+        enhancerString = '%s:%s-%s' % (line[1],line[2],line[3])
+        
+        enhancerLocus = ROSE_utils.Locus(line[1],line[2],line[3],'.',line[0])
+
+        #overlapping genes are transcribed genes whose transcript is directly in the stitchedLocus         
+        overlappingLoci = transcribedCollection.getOverlap(enhancerLocus,'both')           
+        overlappingGenes =[]
+        for overlapLocus in overlappingLoci:                
+            overlappingGenes.append(overlapLocus.ID())
+
+        #proximalGenes are transcribed genes where the tss is within 50kb of the boundary of the stitched loci
+        proximalLoci = tssCollection.getOverlap(ROSE_utils.makeSearchLocus(enhancerLocus,50000,50000),'both')           
+        proximalGenes =[]
+        for proxLocus in proximalLoci:
+            proximalGenes.append(proxLocus.ID())
+
+
+        distalLoci = tssCollection.getOverlap(ROSE_utils.makeSearchLocus(enhancerLocus,50000000,50000000),'both')           
+        distalGenes =[]
+        for proxLocus in distalLoci:
+            distalGenes.append(proxLocus.ID())
+
+            
+            
+        overlappingGenes = ROSE_utils.uniquify(overlappingGenes)
+        proximalGenes = ROSE_utils.uniquify(proximalGenes)
+        distalGenes = ROSE_utils.uniquify(distalGenes)
+        allEnhancerGenes = overlappingGenes + proximalGenes + distalGenes
+        #these checks make sure each gene list is unique.
+        #technically it is possible for a gene to be overlapping, but not proximal since the
+        #gene could be longer than the 50kb window, but we'll let that slide here
+        for refID in overlappingGenes:
+            if proximalGenes.count(refID) == 1:
+                proximalGenes.remove(refID)
+
+        for refID in proximalGenes:
+            if distalGenes.count(refID) == 1:
+                distalGenes.remove(refID)
+
+
+        #Now find the closest gene
+        if len(allEnhancerGenes) == 0:
+            closestGene = ''
+        else:
+            #get enhancerCenter
+            enhancerCenter = (int(line[2]) + int(line[3]))/2
+
+            #get absolute distance to enhancer center
+            distList = [abs(enhancerCenter - startDict[geneID]['start'][0]) for geneID in allEnhancerGenes]
+            #get the ID and convert to name
+            #print enhancerCenter - startDict[geneID]['start'][0]
+            #print distList.index(min(distList))
+            #print min(distList)
+            #print len(distList)
+	    #print len(allEnhancerGenes[distList.index(min(distList))])
+	    #print line
+	    #print len(startDict[allEnhancerGenes[distList.index(min(distList))]])
+            closestGene = startDict[allEnhancerGenes[distList.index(min(distList))]]['name']
+
+        #NOW WRITE THE ROW FOR THE ENHANCER TABLE
+        newEnhancerLine = line[0:6]
+        if byRefseq:
+            newEnhancerLine.append(join(ROSE_utils.uniquify([x for x in overlappingGenes]),','))
+            newEnhancerLine.append(join(ROSE_utils.uniquify([x for x in proximalGenes]),','))
+            #print newEnhancerLine
+            #print len(allEnhancerGenes)
+            #print distList
+            closestGene = allEnhancerGenes[distList.index(min(distList))]
+            newEnhancerLine.append(closestGene)
+        else:
+            newEnhancerLine.append(join(ROSE_utils.uniquify([startDict[x]['name'] for x in overlappingGenes]),','))
+            newEnhancerLine.append(join(ROSE_utils.uniquify([startDict[x]['name'] for x in proximalGenes]),','))
+            closestGene = startDict[allEnhancerGenes[distList.index(min(distList))]]['name']
+            newEnhancerLine.append(closestGene)
+
+
+        newEnhancerLine += line[-2:]
+        enhancerToGeneTable.append(newEnhancerLine)
+        #Now grab all overlapping and proximal genes for the gene ordered table
+
+        overallGeneList +=overlappingGenes
+        for refID in overlappingGenes:
+            geneDict['overlapping'][refID].append(enhancerString)
+        
+
+        overallGeneList+=proximalGenes
+        for refID in proximalGenes:
+            geneDict['proximal'][refID].append(enhancerString)
+
+    #End loop through
+    
+    #Make table by gene
+    overallGeneList = ROSE_utils.uniquify(overallGeneList)  
+
+    nameOrder = ROSE_utils.order([startDict[x]['name'] for x in overallGeneList])
+        
+    usedNames = []
+    for i in nameOrder:
+        refID = overallGeneList[i]
+        geneName = startDict[refID]['name']
+        if usedNames.count(geneName) > 0 and uniqueGenes == True:
+
+            continue
+        else:
+            usedNames.append(geneName)
+        
+        proxEnhancers = geneDict['proximal'][refID] + geneDict['overlapping'][refID]
+        
+    
+        newLine = [geneName,refID,join(proxEnhancers,',')]
+        geneToEnhancerTable.append(newLine)
+
+    #re-sort enhancerToGeneTable
+
+    enhancerOrder = ROSE_utils.order([int(line[-2]) for line in enhancerToGeneTable[1:]])
+    sortedTable = [enhancerToGeneTable[0]]
+    for i in enhancerOrder:
+        sortedTable.append(enhancerToGeneTable[(i+1)])
+
+    return sortedTable,geneToEnhancerTable
+
+
+
+#==================================================================
+#=========================MAIN METHOD==============================
+#==================================================================
+
+def main():
+    '''
+    main run call
+    '''
+    debug = False
+
+
+    from optparse import OptionParser
+    usage = "usage: %prog [options] -g [GENOME] -i [INPUT_ENHANCER_FILE]"
+    parser = OptionParser(usage = usage)
+    #required flags
+    parser.add_option("-i","--i", dest="input",nargs = 1, default=None,
+                      help = "Enter a ROSE ranked enhancer or super-enhancer file")
+    parser.add_option("-g","--genome", dest="genome",nargs = 1, default=None,
+                      help = "Enter the genome build (MM9,MM8,HG18,HG19)")
+
+    #optional flags
+    parser.add_option("-l","--list", dest="geneList",nargs = 1, default=None,
+                      help = "Enter a gene list to filter through")
+    parser.add_option("-o","--out", dest="out",nargs = 1, default=None,
+                      help = "Enter an output folder. Default will be same folder as input file")
+    parser.add_option("-r","--refseq",dest="refseq",action = 'store_true', default=False,
+                      help = "If flagged will write output by refseq ID and not common name")
+
+    #RETRIEVING FLAGS
+    (options,args) = parser.parse_args()
+
+
+    if not options.input or not options.genome:
+
+        parser.print_help()
+        exit()
+
+    #GETTING THE INPUT
+    enhancerFile = options.input
+
+    #making the out folder if it doesn't exist
+    if options.out:
+        outFolder = ROSE_utils.formatFolder(options.out,True)
+    else:
+        outFolder = join(enhancerFile.split('/')[0:-1],'/') + '/'
+
+
+    #GETTING THE GENOME
+    genome = options.genome
+    print('USING %s AS THE GENOME' % genome)
+
+
+    #GETTING THE CORRECT ANNOT FILE
+    cwd = os.getcwd()
+    genomeDict = {
+        'HG18':'%s/annotation/hg18_refseq.ucsc' % (cwd),
+        'MM9': '%s/annotation/mm9_refseq.ucsc' % (cwd),
+        'HG19':'%s/annotation/hg19_refseq.ucsc' % (cwd),
+        'MM8': '%s/annotation/mm8_refseq.ucsc' % (cwd),
+        'MM10':'%s/annotation/mm10_refseq.ucsc' % (cwd),
+        }
+
+    annotFile = genomeDict[upper(genome)]
+
+    #GETTING THE TRANSCRIBED LIST
+    if options.geneList:
+
+        transcribedFile = options.geneList
+    else:
+        transcribedFile = ''
+
+    enhancerToGeneTable,geneToEnhancerTable = mapEnhancerToGene(annotFile,enhancerFile,uniqueGenes=True,byRefseq=options.refseq, transcribedFile=transcribedFile)
+
+    #Writing enhancer output
+    enhancerFileName = enhancerFile.split('/')[-1].split('.')[0]
+
+    #writing the enhancer table
+    out1 = '%s%s_ENHANCER_TO_GENE.txt' % (outFolder,enhancerFileName)
+    ROSE_utils.unParseTable(enhancerToGeneTable,out1,'\t')
+
+    #writing the gene table
+    out2 = '%s%s_GENE_TO_ENHANCER.txt' % (outFolder,enhancerFileName)
+    ROSE_utils.unParseTable(geneToEnhancerTable,out2,'\t')
+
+if __name__ == "__main__":
+    main()
